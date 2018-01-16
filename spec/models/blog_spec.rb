e69fc638257174019ab4ce8b8551c899264e84e8
Harshitha A Rao <hrao20@gmail.com>
2018-01-16 21:30:00 +0530

    Validate on title

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
+  describe '#validations' do
+    it 'should validate presence of title' do
+      blog = Blog.create
+      blog.valid?
+      expect(blog.errors[:title][0]).to eq("can't be blank")
 
+      blog.title = 'sample blog'
+      blog.valid?
+      expect(blog.errors[:title][0]).to eq(nil)
+    end
+  end
 end
