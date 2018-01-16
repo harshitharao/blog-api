6046779de533ddb19950fed36ddbd7e771e26300
Harshitha A Rao <hrao20@gmail.com>
2018-01-16 21:01:02 +0530

    Remove redundant tests

--- spec/requests/blogs_spec.rb
+++ spec/requests/blogs_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Blogs", type: :request do
   describe "GET /blogs" do
-    it "works! (now write some real specs)" do
+    it "returns succesfull response for list of blogs" do
       get blogs_path
       expect(response).to have_http_status(200)
     end
