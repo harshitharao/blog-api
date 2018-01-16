2acb9ff900275e3b2316e2cefb88aff55109b2c1
Harshitha A Rao <hrao20@gmail.com>
2018-01-16 22:35:26 +0530

    Split description to return full step having 30 words

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,3 +1,13 @@
 class BlogSerializer < ActiveModel::Serializer
   attributes :title, :link, :description
+
+  INITIAL_DESCRIPTION_SPLIT=30
+
+  def description
+    description_text = object.description
+    initial_n_words = description_text[/(\s*\S+){#{INITIAL_DESCRIPTION_SPLIT}}/]
+    description_text.slice!(initial_n_words)
+    remaining_description = description_text.split('.')[0]
+    initial_n_words + remaining_description
+  end
 end
--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe BlogsController, type: :controller do
   let(:valid_attributes) {
     {
       title: 'Sample blog',
-      link: 'https://sample-blog.html'
+      link: 'https://sample-blog.html',
+      description: 'I want to change but my past haunts me, Swami,&#8221; a visitor said to me recently. &#8220;I constantly feel guilty for my sins. How do I get rid of my baggage.Two things will follow you to your grave. I replied. &#8220;Wanna guess?&#8221; And creditors,&#8221; I joked. He laughed a nervous laugh.That is not to say that there&#8217;s no way of shedding our past. '
     }
   }
 
@@ -16,12 +17,10 @@ RSpec.describe BlogsController, type: :controller do
     }
   }
 
-  let(:valid_session) { {} }
-
   describe "GET #index" do
     it "returns a success response" do
       blog = Blog.create! valid_attributes
-      get :index, params: {}, session: valid_session
+      get :index, params: {}
       expect(response).to be_success
     end
   end
@@ -29,8 +28,15 @@ RSpec.describe BlogsController, type: :controller do
   describe "GET #show" do
     it "returns a success response" do
       blog = Blog.create! valid_attributes
-      get :show, params: {id: blog.to_param}, session: valid_session
+      get :show, params: {id: blog.to_param}
       expect(response).to be_success
+    end
+
+    it "returns blog having description around 30 words" do
+      blog = Blog.create! valid_attributes
+      get :show, params: {id: blog.to_param}
+      response_body = JSON.parse(response.body)
+      expect(response_body['description']).to eq('I want to change but my past haunts me, Swami,&#8221; a visitor said to me recently. &#8220;I constantly feel guilty for my sins. How do I get rid of my baggage')
     end
   end
 
@@ -38,12 +44,12 @@ RSpec.describe BlogsController, type: :controller do
     context "with valid params" do
       it "creates a new Blog" do
         expect {
-          post :create, params: {blog: valid_attributes}, session: valid_session
+          post :create, params: {blog: valid_attributes}
         }.to change(Blog, :count).by(1)
       end
 
       it "renders a JSON response with the new blog" do
-        post :create, params: {blog: valid_attributes}, session: valid_session
+        post :create, params: {blog: valid_attributes}
         expect(response).to have_http_status(:created)
         expect(response.content_type).to eq('application/json')
         expect(response.location).to eq(blog_url(Blog.last))
@@ -52,7 +58,7 @@ RSpec.describe BlogsController, type: :controller do
 
     context "with invalid params" do
       it "renders a JSON response with errors for the new blog" do
-        post :create, params: {blog: invalid_attributes}, session: valid_session
+        post :create, params: {blog: invalid_attributes}
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response.content_type).to eq('application/json')
       end
