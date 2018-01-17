c94bd70d2b7fe058e64688ccabca894e6bfeb8b7
Harshitha A Rao <hrao20@gmail.com>
2018-01-17 11:10:10 +0530

    Optimise blogs creation from rss feed with tests

--- app/helpers/task_helper.rb
+++ app/helpers/task_helper.rb
@@ -1,7 +1,10 @@
 module TaskHelper
   def self.create_new_blogs_from_feed(rss_feeds)
-    blog_dates = Blog.all.map(&:published_date)
-    feeds_with_new_dates = rss_feeds.select{|feed| !blog_dates.include?(feed[:published_date])}
+    last_blog = Blog.last
+    last_blog_published_date = last_blog && last_blog.published_date
+    feeds_with_new_dates = last_blog_published_date ?
+      rss_feeds.select{|feed| feed[:published_date] > last_blog_published_date} :
+      rss_feeds
     Blog.create(feeds_with_new_dates)
   end
 end
--- spec/helpers/task_helper_spec.rb
+++ spec/helpers/task_helper_spec.rb
@@ -22,5 +22,71 @@ RSpec.describe TaskHelper do
       expect(Blog.count).to eq(2)
       expect(Blog.all.map(&:title)).to eq(['blog 2', 'blog 1'])
     end
+
+    it "returns all new blogs after the last blogs published date of the db" do
+      rss_feeds = [
+        {
+          title: 'blog 1',
+          link: 'http://blog1.html',
+          published_date: 'Tue, 19 Oct 2004 11:09:03 -0400'
+        },
+        {
+          title: 'blog 2',
+          link: 'http://blog2.html',
+          published_date: 'Tue, 18 Oct 2004 11:09:03 -0400'
+        },
+        {
+          title: 'blog 3',
+          link: 'http://blog3.html',
+          published_date: 'Tue, 17 Oct 2004 11:09:03 -0400'
+        }
+      ]
+      blog = Blog.create!(title: 'blog 3', link: 'http://blog3.html', published_date: 'Tue, 17 Oct 2004 11:09:03 -0400')
+      expect(Blog.count).to eq(1)
+      expect(Blog.all.map(&:title)).to eq(['blog 3'])
+      TaskHelper.create_new_blogs_from_feed(rss_feeds)
+      expect(Blog.count).to eq(3)
+      expect(Blog.all.map(&:title)).to eq(['blog 3', 'blog 1', 'blog 2'])
+    end
+
+    it "doesnt create new blogs if there are no new feeds" do
+      rss_feeds = [
+        {
+          title: 'blog 3',
+          link: 'http://blog3.html',
+          published_date: 'Tue, 17 Oct 2004 11:09:03 -0400'
+        }
+      ]
+      blog = Blog.create!(title: 'blog 3', link: 'http://blog3.html', published_date: 'Tue, 17 Oct 2004 11:09:03 -0400')
+      expect(Blog.count).to eq(1)
+      expect(Blog.all.map(&:title)).to eq(['blog 3'])
+      TaskHelper.create_new_blogs_from_feed(rss_feeds)
+      expect(Blog.count).to eq(1)
+      expect(Blog.all.map(&:title)).to eq(['blog 3'])
+    end
+
+    it "creates all blogs from feed when blog is empty" do
+      rss_feeds = [
+        {
+          title: 'blog 3',
+          link: 'http://blog3.html',
+          published_date: 'Tue, 17 Oct 2004 11:09:03 -0400'
+        }
+      ]
+      expect(Blog.count).to eq(0)
+      TaskHelper.create_new_blogs_from_feed(rss_feeds)
+      expect(Blog.count).to eq(1)
+      expect(Blog.all.map(&:title)).to eq(['blog 3'])
+    end
+
+    it "doesnt create any blog if the feed is empty" do
+      rss_feeds = []
+      blog = Blog.create!(title: 'blog 3', link: 'http://blog3.html', published_date: 'Tue, 17 Oct 2004 11:09:03 -0400')
+      expect(Blog.count).to eq(1)
+      expect(Blog.all.map(&:title)).to eq(['blog 3'])
+      TaskHelper.create_new_blogs_from_feed(rss_feeds)
+      expect(Blog.count).to eq(1)
+      expect(Blog.all.map(&:title)).to eq(['blog 3'])
+    end
   end
 end
