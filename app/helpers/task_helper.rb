a70cd5cd3bcb1969427a4280c7babdf54532f673
Harshitha A Rao <hrao20@gmail.com>
2018-01-17 12:41:14 +0530

    Compare published dates of blogs and rss feeds

--- app/helpers/task_helper.rb
+++ app/helpers/task_helper.rb
@@ -1,10 +1,7 @@
 module TaskHelper
   def self.create_new_blogs_from_feed(rss_feeds)
-    last_blog = Blog.last
-    last_blog_published_date = last_blog && last_blog.published_date
-    feeds_with_new_dates = last_blog_published_date ?
-      rss_feeds.select{|feed| feed[:published_date] > last_blog_published_date} :
-      rss_feeds
+    blog_dates = Blog.all.map(&:published_date).uniq
+    feeds_with_new_dates = rss_feeds.select{|feed| !blog_dates.include?(feed[:published_date])}
     Blog.create(feeds_with_new_dates)
   end
 end
--- spec/helpers/task_helper_spec.rb
+++ spec/helpers/task_helper_spec.rb
@@ -88,5 +88,49 @@ RSpec.describe TaskHelper do
       expect(Blog.count).to eq(1)
       expect(Blog.all.map(&:title)).to eq(['blog 3'])
     end
+
+    it "doesnt create any blog if the feed is invalid" do
+      rss_feeds = [
+        {
+          link: 'http://blog3.html',
+          published_date: 'Tue, 17 Oct 2004 11:09:03 -0400'
+        }
+      ]
+      expect(Blog.count).to eq(0)
+      TaskHelper.create_new_blogs_from_feed(rss_feeds)
+      expect(Blog.count).to eq(0)
+    end
+
+    it "returns new blogs based on latest blog published date" do
+      rss_feed = [
+        {
+          title: 'blog 1',
+          link: 'http://blog1.html',
+          published_date: 'Tue, 19 Oct 2004 11:09:03 -0400'
+        },
+        {
+          title: 'blog 4',
+          link: 'http://blog4.html',
+          published_date: 'Tue, 19 Oct 2004 11:09:03 -0400'
+        },
+        {
+          title: 'blog 2',
+          link: 'http://blog2.html',
+          published_date: 'Tue, 18 Oct 2004 12:09:03 -0400'
+        },
+        {
+          title: 'blog 3',
+          link: 'http://blog3.html',
+          published_date: 'Tue, 18 Oct 2004 11:09:03 -0400'
+        }
+      ]
+      blog = Blog.create!(title: 'blog 2', link: 'http://blog2.html', published_date: 'Tue, 18 Oct 2004 12:09:03 -0400')
+      blog = Blog.create!(title: 'blog 3', link: 'http://blog3.html', published_date: 'Tue, 18 Oct 2004 11:09:03 -0400')
+      expect(Blog.count).to eq(2)
+      expect(Blog.all.map(&:title)).to eq(['blog 2', 'blog 3'])
+      TaskHelper.create_new_blogs_from_feed(rss_feed)
+      expect(Blog.count).to eq(4)
+      expect(Blog.all.map(&:title)).to eq(['blog 2', 'blog 3', 'blog 1', 'blog 4'])
+    end
   end
 end
